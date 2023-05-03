d2538908daba00fac316c3b42b83f05085a58280
napolitanacc <beatriz.galan.conde@alumnos.upm.es>
2023-05-03 10:20:22 +0200

    Update AND.cpp

--- AND.cpp
+++ AND.cpp
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#define NUM_FUENTES 25
 
-struct TFuente{
+struct TFuente{   //Creamos una estructura que nos permita tener acceso a todos los todos los campos de las fuentes
 	char nombre[50];
 	float ph;
-	float conductividad;
 	float turbidez;
+	float conductividad;
 	int coliformes;
 };
 
-
-
 int main(){
 	
-	FILE *fichero = fopen("DATOS.txt", "r"); //Utilizamos este arreglo para leer el contenido del fichero
-	if (fichero == NULL) {
-    printf("No se pudo abrir el archivo.\n");
-    return 0;
-}
+	FILE *fichero=fopen("DATOS.txt", "r"); //Los datos están guardados en un fichero, queremos abrir el fichero para poder hacer referencia a ellos
+	if(fichero==NULL){
+		printf("ERROR, no se pudo abrir el fichero correctamente\n");
+		return 0;
+	}
+	
+	struct TFuente barrio[NUM_FUENTES];
+	
+	
+	
 	int opcion;
 	do{
 	printf("Bienvenidos a AquaLife\n");
