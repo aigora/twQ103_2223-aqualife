70711af19a532de0f087d7885bbdc477197a64b7
napolitanacc <beatriz.galan.conde@alumnos.upm.es>
2023-05-17 13:00:28 +0200

    Update AND.cpp

--- AND.cpp
+++ AND.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
 #include <stdio.h>
-#define NUM_FUENTES 25
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+#define MAX_ROWS 1000
 
 struct TFuente{   //Creamos una estructura que nos permita tener acceso a todos los todos los campos de las fuentes
 	char nombre[50];
